f39541b39be8ad7bb9273bc070a19b0839fb8ea8
pardhu225 <chowdary1969@gmail.com>
2018-11-14 00:02:57 +0530

    l

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ export let config = {
     RAILWAY_API_KEY: 'm5en0ja33t',
     dev: true,
     useProxy: true ,
-    proxyHttp: 'http://127.0.0.1:40000/'
-}
+    proxyHttp: 'http://172.23.149.112:40000/'
+} 
